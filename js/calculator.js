e7f2a44f67d6500fd5a6584369134714ce736d26
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-11-08 15:51:08 +0100

    Update files :rocket:

---
+++ js/calculator.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Elements
+    const elements = {
+        amount_people: document.getElementById("people"),
+        amount_minutes: document.getElementById("minutes"),
+        saved_water: document.getElementById("water"),
+        saved_money: document.getElementById("money"),
+    };
+
+    // Convert to thousands seperator
+    const formatNumber = (x, fixed) => {
+        return x.toFixed(fixed).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    };
+
+    // Calculate savings
+    const calculateSavings = () => {
+        const flowRate = 9;
+        const showerL = elements.amount_minutes.value * flowRate;
+
+        const waterCost = 4;
+        const waterCostL = waterCost / 1000;
+        const waterCostShower = waterCostL * showerL;
+
+        const heat = 0.04;
+        const heatCostKwh = 0.4;
+        const heatCostL = heatCostKwh * heat;
+        const heatCostShower = showerL * heatCostL;
+
+        const totalCostShower = heatCostShower +  waterCostShower;
+
+        const savingShowerMoney = totalCostShower * 0.3;
+        const savingShowerMoneyYear = savingShowerMoney * 365;
+
+        const savingShowerLiter = showerL * 0.3;
+        const savingShowerLiterYear = savingShowerLiter * 365;
+
+        elements.saved_water.innerHTML = formatNumber(savingShowerLiterYear, 0) + "L";
+        elements.saved_money.innerHTML = "£" + formatNumber(savingShowerMoneyYear *(elements.amount_people.value ?? 0), 0);
+    };
+
+    // Change on every input change
+    elements.amount_people.addEventListener("input", () => calculateSavings());
+    elements.amount_minutes.addEventListener("input", () => calculateSavings());
+});
---
+++ js/shop.js
@@ -0,0 +1,177 @@
+document.addEventListener("DOMContentLoaded", () => {
+    /**
+     * Change thumbnails
+     */
+
+    // Elements
+    const elements = {
+        mainImage: document.getElementById("main-image"),
+        thumbnails: document.querySelectorAll(".thumbnail"),
+    };
+    
+    elements.thumbnails.forEach((thumbnail) => {
+        thumbnail.addEventListener("click", () => {
+            mainImage.src = thumbnail.src;
+        });
+    });
+
+    /**
+     * Add to cart - create list item
+     */
+
+    // Elements
+    const finalPrice = document.getElementById("total-price");
+
+    // Variables
+    let arrayOfItems = [];
+    let totalPrice = 0;
+
+    const updateList = (items) => {
+        arrayOfItems.forEach((item) => {
+            item.remove();
+        });
+
+        items.forEach((item) => {
+            const listItemWrapper = document.createElement("div");
+            listItemWrapper.classList.add("list-item");
+
+            const listItemName = document.createElement("span");
+            listItemName.classList.add("list-item-name");
+            listItemName.innerHTML = `${item.name} | ${item.amount}x`;
+
+            const listItemPrice = document.createElement("span");
+            listItemPrice.classList.add("list-item-price");
+            listItemPrice.innerHTML = `£${item.price}`;
+
+            listItemWrapper.appendChild(listItemName);
+            listItemWrapper.appendChild(listItemPrice);
+
+            totalPrice = totalPrice + item.price;
+        });
+
+        finalPrice.innerHTML = `£${totalPrice}`;
+    };
+
+    /**
+     * General - amount selector
+     */
+
+    const minusAmount = (minus, amount) => {
+        if (amount !== 0) {
+            const updatedAmount = Number(amount.innerHTML) - 1;
+            amount.innerHTML = updatedAmount;
+
+            if (updatedAmount === 0) {
+                minus.classList.add("inactive");
+            };
+
+            return updatedAmount;
+        } else {
+            return 0;
+        };
+    };
+
+    const plusAmount = (minus, amount) => {
+        const updatedAmount = Number(amount.innerHTML) + 1;
+        amount.innerHTML = updatedAmount;
+
+        if (updatedAmount !== 0) {
+            minus.classList.remove("inactive");
+        };
+
+        return updatedAmount;
+    };
+
+    /**
+     * Main product - amount selector
+     */
+
+    // Elements
+    const minusMain = document.getElementById("minus-main");
+    const plusMain = document.getElementById("plus-main");
+    const amountMain = document.getElementById("amount-main");
+    const priceMain = 89;
+
+    minusMain.addEventListener("click", () => {
+        const amount = minusAmount(minusMain, amountMain);
+        const price = "£" + Number(priceMain * amount).toFixed(2);
+
+        for (let i = 0; i < arrayOfItems.length; i++) {
+            if (amount === 0) {
+                if (arrayOfItems[i].name === "Discovery Package") {
+                    arrayOfItems.splice(i, 1);
+                };
+            } else {
+                arrayOfItems[i].amount = amount;
+                arrayOfItems[i].price = price;
+            };
+        };
+    });
+
+    plusMain.addEventListener("click", () => {
+        const amount = plusAmount(minusMain, amountMain);
+        const price = "£" + Number(priceMain * amount).toFixed(2);
+
+        for (let i = 0; i < arrayOfItems.length; i++) {
+            if (amount === 1) {
+                arrayOfItems.push({
+                    name: "Discovery Package",
+                    amount: amount,
+                    price: price
+                });
+            } else {
+                arrayOfItems[i].amount = amount;
+                arrayOfItems[i].price = price;  
+            };
+        };
+    });
+
+    /**
+     * Refill - amount selector
+     */
+
+    // Elements
+    const refillBoxes = document.querySelectorAll(".refill-box");
+
+    refillBoxes.forEach((refill) => {
+        const nameRefill = refill.querySelector(".name-refill");
+        const minusRefill = refill.querySelector("minus-refill");
+        const plusRefill = refill.querySelector("plus-refill");
+        const amountRefill = refill.querySelector("amount-refill");
+        const priceRefill = 5;
+
+        minusRefill.addEventListener("click", () => {
+            const amount = minusAmount(minusRefill, amountRefill);
+            const price = "£" + Number(priceRefill * amount).toFixed(2);
+
+            for (let i = 0; i < arrayOfItems.length; i++) {
+                if (amount === 0) {
+                    if (arrayOfItems[i].name === nameRefill.innerHTML) {
+                        arrayOfItems.splice(i, 1);
+                    };
+                } else {
+                    arrayOfItems[i].amount = amount;
+                    arrayOfItems[i].price = price;
+                };
+            };
+        });
+
+        plusRefill.addEventListener("click", () => {
+            const amount = plusAmount(minusRefill, amountRefill);
+            const price = "£" + Number(priceRefill * amount).toFixed(2);
+    
+            for (let i = 0; i < arrayOfItems.length; i++) {
+                if (amount === 1) {
+                    arrayOfItems.push({
+                        name: nameRefill.innerHTML,
+                        amount: amount,
+                        price: price
+                    });
+                } else {
+                    arrayOfItems[i].amount = amount;
+                    arrayOfItems[i].price = price;  
+                };
+            };
+        });
+    });
+});
